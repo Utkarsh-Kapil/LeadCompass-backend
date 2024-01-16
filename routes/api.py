133b971fffaa7cd33cb7b5887194d50db0eec457
ujdemon777 <Ujjwal@shorthillstech.com>
2024-01-16 17:39:14 +0530

    project code optimised

--- routes/api.py
+++ routes/api.py
@@ -1,10 +1,9 @@
 from fastapi import APIRouter
-from endpoints import auth, user, sam, validate, project, module, prospects, upload, contact
+from endpoints import auth, user, validate, project, module, prospects, upload, contact
 
 router = APIRouter()
 router.include_router(auth.router)
 router.include_router(user.router)
-router.include_router(sam.router)
 router.include_router(validate.router)
 router.include_router(project.router)
 router.include_router(module.router)
